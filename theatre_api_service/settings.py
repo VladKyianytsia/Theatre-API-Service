94079331cc6a2f8f16d97ad1e3c4060bcff109e6
Vlad Kyianytsia <kiyanitsa2002021@gmail.com>
2024-03-27 14:50:44 +0200

    added JWT Token authentication

--- theatre_api_service/settings.py
+++ theatre_api_service/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "rest_framework",
+    "rest_framework.authtoken",
     "theatre",
     "user",
 ]
@@ -127,3 +128,9 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "user.User"
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+}
