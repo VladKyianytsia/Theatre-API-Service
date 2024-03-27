1e9efa8fd04df5ca2199308b150636f5e67765f0
Vlad Kyianytsia <kiyanitsa2002021@gmail.com>
2024-03-27 11:17:12 +0200

    added base viewsets for each model

--- theatre/views.py
+++ theatre/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from theatre.models import (
+    TheatreHall,
+    Genre,
+    Actor,
+    Play,
+    Performance,
+    Reservation,
+    Ticket
+)
+from theatre.serializers import (
+    TheatreHallSerializer,
+    GenreSerializer,
+    ActorSerializer,
+    PlaySerializer,
+    PerformanceSerializer,
+    ReservationSerializer,
+    TicketSerializer
+)
+
+
+class TheatreHallViewSet(viewsets.ModelViewSet):
+    queryset = TheatreHall.objects.all()
+    serializer_class = TheatreHallSerializer
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+
+class ActorViewSet(viewsets.ModelViewSet):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
+
+
+class PlayViewSet(viewsets.ModelViewSet):
+    queryset = Play.objects.all()
+    serializer_class = PlaySerializer
+
+
+class PerformanceViewSet(viewsets.ModelViewSet):
+    queryset = Performance.objects.all()
+    serializer_class = PerformanceSerializer
+
+
+class ReservationViewSet(viewsets.ModelViewSet):
+    queryset = Reservation.objects.all()
+    serializer_class = ReservationSerializer
+
+
+class TicketViewSet(viewsets.ModelViewSet):
+    queryset = Ticket.objects.all()
+    serializer_class = TicketSerializer
